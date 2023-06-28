ac2ea7fc7153aa07284edf63cafd4b4e612d3f85
Ignacio Gramajo Feijoo <118638937+3003mgf@users.noreply.github.com>
2023-06-28 05:18:43 -0300

    Commit1

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const app = require("./app");
-//const port = app.get("port");
+const port = app.get("port");
 const sequelize = require("./src/database/sequelize");
 
 
 const main = async() =>{
   try{
     await sequelize.sync({force: false});
-    app.listen(3001, ()=>{
-      console.log("Server running succesfully at port %s")
+    app.listen(port, ()=>{
+      console.log("Server running succesfully at port %s", `${port}!`)
     })
   }catch(error){
     console.log("Unable to connect to the Database :(", error)
