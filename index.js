8dbb4b5f493bcbeffc04aa4c006806eec5001082
Ignacio Gramajo Feijoo <118638937+3003mgf@users.noreply.github.com>
2023-06-28 05:14:09 -0300

    problem

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const app = require("./app");
-const port = app.get("port");
+//const port = app.get("port");
 const sequelize = require("./src/database/sequelize");
 
 
 const main = async() =>{
   try{
     await sequelize.sync({force: false});
-    app.listen(port, ()=>{
+    app.listen(3001, ()=>{
       console.log("Server running succesfully at port %s", `${port}!`)
     })
   }catch(error){
