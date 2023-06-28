37a3ff4f67db32a57f48c18a6f8566ba1378a773
Ignacio Gramajo Feijoo <118638937+3003mgf@users.noreply.github.com>
2023-06-28 05:15:37 -0300

    problem2

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const main = async() =>{
   try{
     await sequelize.sync({force: false});
     app.listen(3001, ()=>{
-      console.log("Server running succesfully at port %s", `${port}!`)
+      console.log("Server running succesfully at port %s")
     })
   }catch(error){
     console.log("Unable to connect to the Database :(", error)
